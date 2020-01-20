c49cb5e1d83cc58a3a24cf8d4eef72a0fc8eeef6
mdonic <mdonic92@gmail.com>
2020-01-20 20:17:29 +0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # testcase_darksky
+## Call *darksky_script.py*
+### without args: current weather in the *cities* table
+`python darksky_apiscript.py`
+### 10 min weather in the city (int city_id in the testcase from 1 to 12)
+`python darksky_apiscript.py --city_id {city_id}`
+### convert to csv:
+`python darksky_apiscript.py --fname {filename.csv}`
--- darksky_apiscript.py
+++ darksky_apiscript.py
@@ -23,18 +23,31 @@ class ApiError(Exception):
 
 
 def write_weather_to_db(data, city_id, time_tag=nowtime.timestamp()):
+    '''
+    insert data from api query to db
+    :param data: api query result
+    :param city_id: int id of required city
+    :param time_tag: timestamp for minutely forecast
+    :return:
+    '''
     try:
         cur = db_connect.cursor()
         query = '''Insert into weather(time, summary, windSpeed, temperature, uvIndex, visibility, city_id) 
                     values (?,?,?,?,?,?,?);'''
         cur.execute(query,(time_tag, data['summary'], data['windSpeed'], data['temperature'], data['uvIndex'], data['visibility'], city_id,))
         res = cur.fetchall()
+        db_connect.commit()
         logging.info(res)
     except Exception as e:
         logging.exception(e)
 
 
 def check_interval(city_id):
+    '''
+
+    :param city_id: int id of required city
+    :return:
+    '''
     try:
         cur = db_connect.cursor()
         query = '''Select MAX(time) 
@@ -42,13 +55,20 @@ def check_interval(city_id):
                     where city_id=?'''
         cur.execute(query,(city_id,))
         res = cur.fetchall()
-        logging.info(res)
+        return res[0][0]
     except Exception as e:
         logging.exception(e)
 
 
 def query_to_api(lat, lon, time_tag=''):
-    if len(time_tag) > 0:
+    '''
+
+    :param lat: lattitude
+    :param lon: longtitude
+    :param time_tag: timestamp for arg in api query
+    :return: data from api query
+    '''
+    if len(str(time_tag)) > 0:
         url = 'https://api.darksky.net/forecast/' + darksky_api_key + '/' + str(lat) + ',' + str(lon) + ',' + str(time_tag) + '?'+ 'units=si'
     else:
         url = 'https://api.darksky.net/forecast/' + darksky_api_key + '/' + str(lat) + ',' + str(lon) + '?' + 'units=si'
@@ -61,6 +81,12 @@ def query_to_api(lat, lon, time_tag=''):
 
 
 def get_weather_by_city_id(city_id, time_tag=0):            #input city name (city_id)
+    '''
+
+    :param city_id: int id of required city
+    :param time_tag:
+    :return: data from db for required city
+    '''
     try:
         cur = db_connect.cursor()
         if time_tag == 0:
@@ -81,6 +107,10 @@ def get_weather_by_city_id(city_id, time_tag=0):            #input city name (ci
 
 
 def get_all_cities():
+    '''
+
+    :return: all cities fields value
+    '''
     try:
         cur = db_connect.cursor()
         query = '''Select * from cities;'''
@@ -92,35 +122,49 @@ def get_all_cities():
 
 
 def get_10_mins_avg_weather(city_id):
+    '''
 
-    c = get_city_info_by_id(city_id)
+    :param city_id: int id of required city
+    :return: print min max avg temp
+    '''
+    res = []
+    c = get_city_info_by_id(city_id)[0]
     x = prettytable.PrettyTable(
         ['City', 'min_temperature', 'max_temperature', 'avg_temperature'])
-    x.set_field_align("City", "l")
-    x.set_padding_width(1)
     if (int(nowtime.timestamp()) - int(check_interval(city_id)) >= 60):
         for i in range(10):
-            q = query_to_api(c[2], c[3], (nowtime-(i*time_delta)).timestamp())
-            write_weather_to_db(q['currently'], c[0], (nowtime-(i*time_delta)).timestamp())
-    res = get_weather_by_city_id(city_id, 1)
+            q = query_to_api(c[2], c[3], int((nowtime-(i*time_delta)).timestamp()))
+            # write_weather_to_db(q['currently'], c[0], (nowtime-(i*time_delta)).timestamp())
+            res.append(float(q['currently']['temperature']))
+    # res = get_weather_by_city_id(city_id, 1)
     values = find_min_max_avg(res)
     x.add_row([c[1],values[0], values[1], values[2]])
     print(x)
 
 
 def find_min_max_avg(res_query):
-    min_v = min(res_query)[3]
-    max_v = max(res_query)[3]
-    avg_v = sum(res_query)[3]/len(res_query)
+    '''
+
+    :param res_query: data for process
+    :return: min max avg
+    '''
+    min_v = min(res_query)
+    max_v = max(res_query)
+    avg_v = sum(res_query)/len(res_query)
     return [min_v, max_v, avg_v]
 
 
 def get_city_info_by_id (city_id):
+    '''
+
+    :param city_id: int id of required city
+    :return: fields values
+    '''
     try:
         cur = db_connect.cursor()
         query = '''Select * 
                     from cities 
-                    where cities.id=?;'''
+                    where cities.city_id=?;'''
         cur.execute(query, (city_id,))
         res = cur.fetchall()
         return res
@@ -137,7 +181,7 @@ def get_current_weather():
     x = prettytable.PrettyTable()
     x.field_names = ['City', 'time', 'summary', 'windSpeed', 'temperature', 'uvIndex', 'visibility', 'city_id']
     for c in cities:
-        if (check_interval(c[0])):
+        if (check_interval(c[0]) is not None):
             if (int(nowtime.timestamp()) - int(check_interval(c[0])) >= 60):
                 q = query_to_api(c[2], c[3])
                 write_weather_to_db(q['currently'], c[0])
@@ -150,6 +194,11 @@ def get_current_weather():
 
 
 def export_to_csv(filename):
+    '''
+
+    :param filename: output filename
+    :return:
+    '''
     try:
         cur = db_connect.cursor()
         query = '''Select cities.name AS cname, weather.time, summary, windSpeed, temperature, uvIndex, visibility, weather.city_id 
@@ -167,7 +216,20 @@ def export_to_csv(filename):
     pass
 
 
-if __name__ == '__main__':
-    get_current_weather()
+def main(*args, **kwargs):
+    parser = argparse.ArgumentParser()
 
-    # pprint.pprint(query_to_api(q[0][0], q[0][1]))
+    parser.add_argument('--fname')
+    parser.add_argument('--city_id')
+
+    args=parser.parse_args()
+    if args.fname is not None:
+        export_to_csv(args.fname)
+    elif args.city_id is not None:
+        get_10_mins_avg_weather(args.city_id)
+    else:
+        get_current_weather()
+
+
+if __name__ == '__main__':
+    main()
